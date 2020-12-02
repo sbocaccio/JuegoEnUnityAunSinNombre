43d80c48436ca166a3f2d9bdaca16bfa4e525ab9
sebasbocaccio <sebastianbocaccio16@gmail.com>
2020-12-02 15:52:12 -0800

    Add basic movement of maincaracter

---
+++ Assets/Movimiento.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Movimiento : MonoBehaviour
+{
+    public float velocidad = 0.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementX = Input.GetAxis("Horizontal");
+        float movementY = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(movementX,movementY,0) * velocidad * Time.deltaTime);
+    }
+}
