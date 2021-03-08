426a6274d5b0985f3c8ba23a082c66df7abb2e79
sebasbocaccio <sebastianbocaccio16@gmail.com>
2021-03-08 18:32:42 -0300

    Add AudoManagement

---
+++ Assets/Audiotest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audiotest : MonoBehaviour
+{
+
+    public AudioManager audio_manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        audio_manager.Play("BackGroundMusic");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
